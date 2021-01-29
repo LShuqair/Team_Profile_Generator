82f8ab82cbcaa3d743906ed9244d39b7e95566a9
Luna Shuqair <lunashuqair@Lunas-MacBook-Pro.local>
2021-01-28 23:24:38 -0500

    working with Zaid, Menuka, Ikra on this assignment

--- Develop/page.template.js
+++ Develop/page.template.js
@@ -6,7 +6,7 @@ const generateTeam = team => {
     // create the manager html
     const generateManager = manager => {
         return `
-        <div class = "card employee-card">
+        <div class = "card employee-card" id="container">
             <div class = "card-header">
                 <h2 class = "card-title">${manager.getName()}</h2>
                 <h3 class = "card-title"><i class = "fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
@@ -85,11 +85,11 @@ const generateTeam = team => {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>My Team</title>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-                <link rel="stylesheet" href="style.css">
+                <link rel="stylesheet" href="../stylesheet.css">
                 <script src="https://kit.fontawesome.com/71dcd7ccb8.js" crossorigin="anonymous"></script>
             </head>
             <body>
-                <div class="container-fluid">
+                <div class="container-fluid" id="container">
                  <div class="row'>
                     <div class="team-area col-12 d-flex justify-content-center">
                     ${generateTeam(team)}
